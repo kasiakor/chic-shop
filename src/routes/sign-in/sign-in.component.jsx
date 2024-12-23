512c3f5d131595dbe78dddff5fb3b38c9d516eec
kasia.kordys <kasia.kordys@gmail.com>
2024-12-23 20:37:14 +0200

    add firebase with config

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,17 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <div>This is to sign in</div>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log("response", response);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
