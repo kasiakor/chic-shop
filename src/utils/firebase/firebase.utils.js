12363db3ce991a9efada26edcc37ce2f713a00f6
kasia.kordys <kasia.kordys@gmail.com>
2024-12-28 23:27:37 +0200

    add a way to create users inl auth and storage using firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,3 +25,44 @@ googleProvider.setCustomParameters({
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
+
+// Instatiate database
+export const db = getFirestore();
+
+// get authentication object and store it in the firestore
+
+// check if there is there is existing document reference
+// / object/instance of document model
+
+// use uid on the sing-in response for user as doc identifier
+// set and get data using reference
+export const createUserDocumentFromAuth = async (userAuth) => {
+  if (!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot);
+  // console.log(userSnapshot.exists());
+  // false, does not exist yet in db
+
+  // if user data does not exist
+  // create/ set the document with data from userAuth in my collection
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+  // if user data exists
+  // return userDocRef
+  return userDocRef;
+};
