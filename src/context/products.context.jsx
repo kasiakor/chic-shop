5b1a8a3a5a467bb85229bbede86a1c8fd59abbff
kasia.kordys <kasia.kordys@gmail.com>
2025-01-05 21:43:40 +0200

    add mock prodcut data and products context

---
+++ src/context/products.context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+// bring products
+import PRODUCTS from "../shop-data.json";
+
+// actual value
+// it will return the object that will be passed as value to Provider, current user
+export const ProductsContext = createContext({
+  products: [],
+});
+
+// provider component
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  const value = { products };
+
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
