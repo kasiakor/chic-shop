83d9b02cc37c1a24f457c9d1cbc05423ee50c822
kasia.kordys <kasia.kordys@gmail.com>
2025-01-14 22:07:00 +0200

    add redux persist to persist cart items

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,32 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { applyMiddleware, compose, createStore } from "redux";
+import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import { rootReducer } from './root-reducer';
+import { rootReducer } from "./root-reducer";
 
+// persist
+
+const persistConfig = {
+  // persist all
+  key: "root",
+  // local storage
+  storage: storage,
+  // exclude, coming from auth listener
+  blacklist: ["user"],
+};
 
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
